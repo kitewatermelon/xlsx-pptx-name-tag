20fccaced737387567939f18eda41668f6c8010d
박연수 <parkite576@gmail.com>
2024-03-21 18:46:38 +0900

    feat:html 파일 연결 완료

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, APIRouter, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(
+    prefix="/nameplate",
+    tags=['nameplate'],
+    responses={404: {"description": "Not found"}}
+)
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
-# 정적 파일을 제공하기 위해 디렉터리 지정
-
-# 루트 엔드포인트
 @app.get("/", response_class=HTMLResponse)
-async def read_index():
-    # HTML 파일의 내용을 읽어 반환
-    with open("index.html", "r") as file:
-        content = file.read()
-    return content
-
-@app.post("/upload")
-async def upload_files(excel_file: UploadFile = File(...), ppt_file: UploadFile = File(...)):
-    excel_file, ppt_file
-    # 업로드된 파일을 처리하는 코드를 여기에 추가
-    return {"excel_file_size": len(await excel_file.read()), "ppt_file_size": len(await ppt_file.read())}
+async def read_all_by_user(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
